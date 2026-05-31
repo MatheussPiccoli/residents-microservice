2f929ef9ed57cc909d1a619a78c4e0fd700c635e
Matheus Piccoli <matheuspdmoura@gmail.com>
2026-05-31 13:52:24 -0300

    feat: add createResidentUseCase and its dependencies

--- src/use cases/create-resident.js
+++ src/use cases/create-resident.js
@@ -1,1 +1,33 @@
-export class CreateResidentUseCase {}
+import { randomUUID } from "crypto";
+import bcrypt from "bcrypt";
+
+export class CreateResidentUseCase {
+  constructor(createResidentRepository, getResidentByEmailRepository) {
+    this.createResidentRepository = createResidentRepository;
+    this.getResidentByEmailRepository = getResidentByEmailRepository;
+  }
+
+  async execute(createResidentParams) {
+    const userWithProvidedEmail = this.getResidentByEmailRepository(
+      createResidentParams.email,
+    );
+
+    if (userWithProvidedEmail) {
+      throw new Error("Email Already in use");
+    }
+
+    const residentId = randomUUID();
+
+    const hashedPassword = bcrypt.hash(createResidentParams.password, 10);
+
+    const resident = {
+      ...createResidentParams,
+      id: residentId,
+      password: hashedPassword,
+    };
+
+    const createdResident = this.createResidentRepository.execute(resident);
+
+    return createdResident;
+  }
+}
