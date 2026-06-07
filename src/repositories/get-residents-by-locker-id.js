2c27232c9fb5796da34599f3e009d166995d3268
Matheus Piccoli <matheuspdmoura@gmail.com>
2026-06-07 16:23:46 -0300

    feat: add PostgresGetResidentsByLockerIdRepository

---
+++ src/repositories/get-residents-by-locker-id.js
@@ -0,0 +1,16 @@
+import { PostgresHelper } from "../db/postgres/helper.js";
+
+export class PostgresGetResidentsByLockerIdRepository {
+  async execute(lockerId) {
+    const residents = await PostgresHelper.query(
+      `
+            SELECT *
+            FROM residents
+            WHERE locker_id = $1
+            `,
+      [lockerId],
+    );
+
+    return residents.rows;
+  }
+}
